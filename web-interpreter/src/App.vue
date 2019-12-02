d9918b114179fa4b0ef2d4c2d082a13c16500b14
junil-hwang <tjsdlf4261@naver.com>
2019-12-02 19:25:55 +0900

    Parser - 프로젝트 initialize 완료

---
+++ web-interpreter/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>웹 인터프리터</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  name: 'App',
+})
+export default class extends Vue {
+  protected async created() {
+    console.log('test');
+  }
+}
+</script>
+
+<style lang="scss" src="@/assets/scss/common.scss" />
