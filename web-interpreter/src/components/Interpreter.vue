75cccd550ff2a04acab3118cec716b6eab47c6fd
junil-hwang <tjsdlf4261@naver.com>
2019-12-17 06:44:29 +0900

    최종 build

--- web-interpreter/src/components/Interpreter.vue
+++ web-interpreter/src/components/Interpreter.vue
@@ -33,44 +33,43 @@ import { eventBus } from '@/Helper';
 
 @Component
 export default class Interpreter extends Vue {
-  private codeList: string[] = [];
-  private code: string = '';
-  private errorText: string = '';
-  private outputList: string[] = [];
-  @Watch('codeList')
+  private codeList: string[] = [];    // input area에 출력될 코드
+  private code: string = '';          // 한 line의 코드
+  private errorText: string = '';     // error에 대한 내용
+  private outputList: string[] = [];  // output area에 출력될 코드
+  @Watch('codeList') // codeList의 변화를 지켜봄
   private onCodeListChange() {
     const { stackContainer }: any = this.$refs;
-    this.$nextTick(() => {
+    this.$nextTick(() => { // 렌더링 후 스크롤을 맨 밑으로 내림
       stackContainer.scrollTo(0, stackContainer.scrollHeight);
     });
   }
-  @Watch('outputList')
+  @Watch('outputList') // outputList의 변화를 지켜봄
   private onOutputListChange() {
     const { outputContainer }: any = this.$refs;
-    this.$nextTick(() => {
+    this.$nextTick(() => { // 렌더링 후 스크롤을 맨 밑으로 내림
       outputContainer.scrollTo(0, outputContainer.scrollHeight);
     });
   }
-  private created() {
+  private created() { // 태그들이 만들어지는 시점에 실행되는 코드
     const { codeList, outputList } = this;
+    // Parser에서 outputAppend를 호출할 경우
     eventBus.$on('outputAppend', (output: string) => {
-      outputList.push(output);
+      outputList.push(output); // output area에 렌더링
     });
-    eventBus.$on('tokenAppend', (token: string) => {
-      codeList.push(token);
-    });
+    // Parser에서 lineAppend를 호출할 경우
     eventBus.$on('lineAppend', (indent: string) => {
-      codeList.push(`${indent}${this.code}`);
-      this.code = '';
-      this.errorText = '';
+      codeList.push(`${indent}${this.code}`); // intput area에 렌더링 후
+      this.code = '';  // 입력중인 코드 초기화
+      this.errorText = ''; // 에러 텍스트 초기화
     });
     eventBus.$on('tokenError', (message: string) => {
-      this.errorText = message;
+      this.errorText = message; // 에러 처리
     });
   }
-  private parsing() {
-    codeContainer.setCode(this.code);
-    checkState();
+  private parsing() { // Parser를 호출하는 코드
+    codeContainer.setCode(this.code); // codeContainer에 현재 입력한 코드를 저장 후
+    checkState(); // checkState를 통해서 parser를 호출
   }
 }
 </script>
